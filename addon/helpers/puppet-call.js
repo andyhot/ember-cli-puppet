36aeebc3950d1a17fdcb09ed7d1e89a3f321cd49
Andreas Andreou <andreoua@gmail.com>
2018-02-17 10:19:26 +0200

    Guard against component not already set

--- addon/helpers/puppet-call.js
+++ addon/helpers/puppet-call.js
@@ -1,6 +1,11 @@
 import { helper } from '@ember/component/helper';
 
+const nop = function() {};
+
 export function puppetCall([component, method, ...args]) {
+  if (!component) {
+    return nop;
+  }
   return component[method].bind(component, ...args);
 }
 
